d54277d64db431168d6c7779b55bc248dccab2eb
Reysil Clarion <rmclarion@PHILKOEI.COM.PH>
2024-05-03 16:12:07 +0800

    Ninth Commit

--- PHP/login.php
+++ PHP/login.php
@@ -28,7 +28,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>License Inventory</title>
-    <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="../CSS/adlogin.css">
     <link rel="icon" href="../IMAGES/BLogo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script defer src="../JS/loginerror.js"></script>
